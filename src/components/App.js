f4668e3b82573ef273d8fa8413dc36d34fd9259f
frontendprof <abdumalksharipov@gmail.com>
2022-02-11 19:08:10 +0500

    Initial setup and cleanup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hola Mundo!!!</header>
+    </div>
+  );
+}
+
+export default App;
